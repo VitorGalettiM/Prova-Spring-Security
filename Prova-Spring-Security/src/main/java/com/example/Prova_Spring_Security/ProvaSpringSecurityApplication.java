1ce740c76311e6c956a8305d97592bc66e2f0b36
Vitinho <vitor.galetti2003@gmail.com>
2025-06-17 20:06:13 -0300

    config bd

---
+++ Prova-Spring-Security/src/main/java/com/example/Prova_Spring_Security/ProvaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prova_Spring_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Prova-Spring-Security/src/test/java/com/example/Prova_Spring_Security/ProvaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prova_Spring_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
